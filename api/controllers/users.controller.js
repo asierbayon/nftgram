b8881b8904e42b0418d040b68374464234e02e61
asierbayon <bayonasier@gmail.com>
2021-04-11 12:24:19 +0200

    fix: retrieve only neccesary data when getting profile

--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -19,7 +19,7 @@ module.exports.create = (req, res, next) => {
 module.exports.get = async (req, res, next) => {
     const { username } = req.params;
     try {
-        const user = await User.findOne({ username });
+        const user = await User.findOne({ username }, 'username fullName id avatar bio website');
         if (!user) next(createError(404, 'User not found'))
 
         const assets = await Asset.find({ owner: user.id })
